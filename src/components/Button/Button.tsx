2506cb7f1080a1e9d35751b9956fe3caf3ddd88c
VituSuperMEg <vitorpires8555@gmail.com>
2024-01-12 07:40:15 -0300

    Colocando as props no button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,17 +1,19 @@
+import { TouchableOpacityProps } from "react-native";
 import { Title } from "../../styles/styles";
 import theme from "../../styles/theme";
 import { ButtonContainer } from "./styles";
 
-interface IButton {
+interface IButton extends TouchableOpacityProps {
   background : keyof typeof theme.colors;
   title : string;
 }
 export function Button ({
   background,
-  title
+  title,
+  ...rest
 }:IButton) {
   return (
-    <ButtonContainer background={background}>
+    <ButtonContainer background={background} {...rest}>
       <Title>
         {title}
       </Title>
