3a6af68d106ba9db9a314f5dc9d579e15ef78035
VituSuperMEg <vitorpires8555@gmail.com>
2024-01-12 11:20:11 -0300

    foodservice complete

--- src/components/FoodService/FoodService.tsx
+++ src/components/FoodService/FoodService.tsx
@@ -2,8 +2,11 @@ import { View } from "react-native";
 import { Title } from "../../styles/styles";
 import { FoodContainer, FoodContent, FoodImg } from "./styles";
 
+import refeicao from "../../assets/images/refeicoes.png";
+import lanches from "../../assets/images/lanches.png";
+import bebidas from "../../assets/images/bebidas.png";
+import buffe from "../../assets/images/buffe.png";
 
-import refeicao from '../../assets/images/refeicoes.png';
 
 export function FoodService() {
   return (
@@ -12,16 +15,32 @@ export function FoodService() {
         <Title>🍳 Serviços</Title>
       </View>
       <FoodContent>
-        <Title>
-          Refeições
-          {"\n"}
-          <Title>A partir de R$ 9,<Title size={10}>99</Title></Title>
-        </Title>
-        <FoodImg source={refeicao}/>
+        <View>
+          <Title size={16}>Refeições</Title>
+          <Title size={25}>
+            A partir de R$ 9, <Title size={20}>99</Title>
+          </Title>
+        </View>
+        <FoodImg source={refeicao} />
       </FoodContent>
-      <FoodContent></FoodContent>
-      <FoodContent></FoodContent>
-      <FoodContent></FoodContent>
+      <FoodContent>
+        <View>
+          <Title size={16}>Lanches</Title>
+        </View>
+        <FoodImg source={lanches} />
+      </FoodContent>
+      <FoodContent>
+        <View>
+          <Title size={16}>Bebidas</Title>
+        </View>
+        <FoodImg source={bebidas} />
+      </FoodContent>
+      <FoodContent>
+        <View>
+          <Title size={16}>Monte seu cardápio</Title>
+        </View>
+        <FoodImg source={buffe} />
+      </FoodContent>
     </FoodContainer>
   );
 }
