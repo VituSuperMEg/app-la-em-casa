752be285b3bfd7e54fa4d6e0b8ac6eba781ca598
VituSuperMEg <vitorpires8555@gmail.com>
2024-01-12 13:26:43 -0300

    create component ref

--- src/screens/Ref/Ref.tsx
+++ src/screens/Ref/Ref.tsx
@@ -1,10 +1,58 @@
+import { ScrollView, View } from "react-native";
 import { Header } from "../../components/Header/Header";
 import { Container, Title } from "../../styles/styles";
+import refeicao from "../../assets/images/refeicoes.png";
+import {
+  RefCardCompletion,
+  RefCart,
+  RefCheckbox,
+  RefCompletion,
+  RefContent,
+  RefImage,
+  RefScroll,
+  RefTitle,
+} from "./styles";
+import { Button } from "../../components/Button/Button";
 
 export function Ref() {
   return (
     <Container>
-     <Header stack/>
+      <Header stack />
+      <RefContent>
+        <RefImage source={refeicao} />
+        <RefTitle>
+          <Title>
+            Escolha o tipo de carne.
+            {"\n"}
+            <Title size={12}>(com direito a dois tipos de carnes)</Title>
+            {"\n"}
+            Acompanha Guarnição, Salada, Feijão e Batata.
+            {"\n"}
+            <Title size={10}>Serve 1 pessoa</Title>
+          </Title>
+        </RefTitle>
+        <RefScroll>
+          <RefCardCompletion>
+            <RefCompletion>
+              <Title>Guarnições</Title>
+            </RefCompletion>
+            <RefCheckbox></RefCheckbox>
+            <RefCheckbox></RefCheckbox>
+          </RefCardCompletion>
+          <RefCardCompletion>
+            <RefCompletion>
+              <Title>Adicionais</Title>
+            </RefCompletion>
+            <RefCheckbox></RefCheckbox>
+            <RefCheckbox></RefCheckbox>
+            <RefCheckbox></RefCheckbox>
+            <RefCheckbox></RefCheckbox>
+          </RefCardCompletion>
+        </RefScroll>
+        <RefCart>
+          <Button background="yellow" title="Adicionar ao Carrinho" height={55}/>
+        </RefCart>
+      </RefContent>
     </Container>
-  )
+  );
 }
---
+++ src/screens/Ref/styles.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components/native";
+import theme from "../../styles/theme";
+
+export const RefContent = styled.View`
+  margin-top: 50px;
+  max-height: 100%;
+  flex: 1;
+`;
+export const RefScroll = styled.ScrollView`
+  flex: 1;
+  width: 100%;
+`;
+export const RefImage = styled.Image`
+  width: 100%;
+  height: 250px;
+`;
+export const RefTitle = styled.View`
+  margin-top: 40px;
+`;
+export const RefCardCompletion = styled.View``;
+export const RefCompletion = styled.View`
+  background: ${theme.colors.yellow};
+  margin-top: 10px;
+  padding-left: 10px;
+  height: 40px;
+  justify-content: center;
+`;
+export const RefCheckbox = styled.TouchableOpacity`
+  background: ${theme.colors.yellowLight};
+  width: 100%;
+  height: 50px;
+  margin-top: 10px;
+  border-radius: 6px;
+`;
+export const RefCart = styled.View`
+  margin: 15px 0;
+`;
