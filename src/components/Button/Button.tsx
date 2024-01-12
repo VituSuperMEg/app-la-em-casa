b6c54d986c1562a71dc2a7a6d15a9fbabc5222e2
VituSuperMEg <vitorpires8555@gmail.com>
2024-01-12 08:00:27 -0300

    custom button e title 100%

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,17 +4,21 @@ import theme from "../../styles/theme";
 import { ButtonContainer } from "./styles";
 
 interface IButton extends TouchableOpacityProps {
-  background : keyof typeof theme.colors;
+  background?: keyof typeof theme.colors;
   title : string;
+  color?: keyof typeof theme.colors;
+  size? : number;
 }
 export function Button ({
   background,
   title,
+  color,
+  size,
   ...rest
 }:IButton) {
   return (
     <ButtonContainer background={background} {...rest}>
-      <Title>
+      <Title color={color} size={size}>
         {title}
       </Title>
     </ButtonContainer>
