9c2a3428a561d8df5bc13c8c072cc240c61eef6a
VituSuperMEg <vitorpires8555@gmail.com>
2024-01-12 17:10:56 -0300

    screen checkout

--- src/screens/Checkout/Checkout.tsx
+++ src/screens/Checkout/Checkout.tsx
@@ -1,11 +1,17 @@
+import { useCart } from "../../state/useCart";
 import { Container, Title } from "../../styles/styles";
 
 export function Checkout() {
+  const cart = useCart((state) => state.cart.flatMap(i => i));
   return (
     <Container>
-     <Title>
-       Check
-     </Title>
+      <Title>Checkout</Title>
+     {cart.map(i => (
+      <>
+      <Title key={i.id}>{i.name}</Title>
+      <Title>R$ {i.price}</Title>
+      </>
+     ))}
     </Container>
   )
 }
