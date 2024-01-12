67e6cc542f5275a8e39af12d04610285a1fa244b
VituSuperMEg <vitorpires8555@gmail.com>
2024-01-12 07:19:44 -0300

    TabComponent 100% Screen

--- src/components/TabComponent/styles.ts
+++ src/components/TabComponent/styles.ts
@@ -2,11 +2,12 @@ import styled from "styled-components/native";
 import theme from "../../styles/theme";
 
 export const TabComponentContainer = styled.View`
-  justify-content: flex-end;
+  width: 120%;
+  margin-left: -20px;
 `;
 export const TabComponentContent = styled.View`
  background :  ${theme.colors.slate};
-border-radius: 6px;
+ border-radius: 6px;
  width : 100%;
  height : 50px;
 `;
