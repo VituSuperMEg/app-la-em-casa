2b53f278bb3b385fa97456798a79bb79c16104ef
VituSuperMEg <vitorpires8555@gmail.com>
2024-01-12 11:39:45 -0300

    new header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,20 +2,42 @@ import { TouchableOpacity, View } from "react-native";
 import { useUser } from "../../state/useUser";
 import { HeaderContainer } from "./styles";
 import { Title } from "../../styles/styles";
-import { Entypo as Icon } from "@expo/vector-icons";
+import { Entypo as Icon, Feather as FA } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
-export function Header() {
+interface IHedaer {
+  stack?: boolean;
+}
+export function Header({ stack = false }: IHedaer) {
+
+  const { goBack } = useNavigation();
+
   const user = useUser((state) => state.user);
 
   return (
     <HeaderContainer>
-      <View>
-        <Title paddingLeft={20}>{user.name}</Title>
-        <Title>📍{user.location}</Title>
-      </View>
-      <TouchableOpacity>
-        <Icon name="menu" size={40} />
-      </TouchableOpacity>
+      {
+        !stack && (
+          <>
+            <View>
+              <Title paddingLeft={20}>{user.name}</Title>
+              <Title>📍{user.location}</Title>
+            </View>
+            <TouchableOpacity>
+              <Icon name="menu" size={40} />
+            </TouchableOpacity>
+          </>
+        )
+      }
+      {
+        stack && (
+          <>
+           <TouchableOpacity onPress={() => goBack()}>
+             <FA name="corner-up-left" size={30} />
+           </TouchableOpacity>
+          </>
+        )
+      }
     </HeaderContainer>
   );
 }
